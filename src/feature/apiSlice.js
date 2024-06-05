808478ddc89596a7f4e9e5dfb03ddccf954bdb38
kmstride <kmstridemission@gmail.com>
2024-06-05 19:50:26 +0600

    backend live link added

--- src/feature/apiSlice.js
+++ src/feature/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const rootApi= createApi({
     reducerPath: "rootApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: `http://localhost:4000/api/v1`,
+        baseUrl: `https://stride-blog-backend.onrender.com/api/v1`,
         prepareHeaders: (headers, { getState }) => {
             const token = getState()?.auth?.token?.payload;
             if (token) {
